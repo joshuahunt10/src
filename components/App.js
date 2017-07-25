9000672d3f8a3f6aea078db38b8ffd42e524d0c1
joshuahunt10 <joshua.hunt88@gmail.com>
2017-07-25 10:08:29 -0500

    initial commit

---
+++ components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+// Import BaseLayout, Appetizers, Entrees, Desserts
+import Appetizers from './Appetizers'
+import BaseLayout from './Layout'
+import Entrees from './Entrees'
+import Desserts from './Desserts'
+
+
+
+class App extends Component {
+// Set initial state for appetizers, entrees, and desserts.
+// All should be set to empty arrays.
+constructor(props){
+super(props)
+  this.state = {
+    appetizers: [],
+    entrees: [],
+    desserts: []
+  }
+}
+
+// Lifecycle method
+// Fetch from http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu.
+// The response should return an object with appetizers, entres, and desserts.
+// Set these to state.
+// YOUR CODE HERE>
+componentDidMount(){
+  fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu')
+  .then(results => results.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+  render() {
+    // Your render should consist of the BaseLayout with the following children components: Appetizers, Entrees, and Dessert.
+    // Each component needs to receive state via props.
+    return (
+
+          <BaseLayout>
+            <Appetizers />
+            <Entrees />
+            <Desserts />
+          </BaseLayout>
+        
+    );
+  }
+}
+
+export default App;
